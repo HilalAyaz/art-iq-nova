7b52c7dfd9357b65f5bb9dbbeefd680ac59126dd
Hilal Ayaz <134634880+HilalAyaz@users.noreply.github.com>
2024-03-15 23:37:53 +0500

    Finished Project

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -78,7 +78,7 @@ const MediaUploader = ({
               </div>
             </>
           ) : (
-            <div className="media-uploader_cta" onClick={uploadProps.open}>
+            <div className="media-uploader_cta" onClick={() => open()}>
               <div className="media-uploader_cta-image">
                 <Image
                   src="/assets/icons/add.svg"
